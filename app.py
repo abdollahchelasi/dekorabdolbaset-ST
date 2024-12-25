812f30e3202e764c4e901e6cee13eaa72bbab0d1
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-12-25 14:32:00 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,27 +8,29 @@ def add_meta_tags():
         # در Hugging Face
         index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
         index_path = index_path.resolve()  # تبدیل به مسیر مطلق
-
         # اگر فایل وجود ندارد، آن را ایجاد کنید
         if not index_path.exists():
             html_content = "<html><head></head><body><h1>خوش آمدید!</h1></body></html>"
-            index_path.write_text(html_content)
+            index_path.write_text(html_content, encoding='utf-8')
 
     except Exception as e:
         # در صورت بروز خطا، ممکن است در Liara یا تغییر مسیر در Hugging Face باشد
         index_path = pathlib.Path("index.html")  # مسیر پیش‌فرض برای Liara
         if not index_path.exists():
             html_content = "<html><head></head><body><h1>خوش آمدید!</h1></body></html>"
-            index_path.write_text(html_content)
+            index_path.write_text(html_content, encoding='utf-8')
 
     # خواندن محتوای HTML
     soup = BeautifulSoup(index_path.read_text(encoding='utf-8'), features="html.parser")
 
     # تغییر عنوان
-    title_tag = soup.new_tag('title')
-    title_tag.string = 'دکوراسیون عبدالباسط - بهترین خدمات دکوراسیون در قشم'
-    soup.head.append(title_tag)
-
+    if soup.find('title'):
+        soup.title.string = 'دکوراسیون عبدالباسط - بهترین خدمات دکوراسیون در قشم'
+    else:
+        title_tag = soup.new_tag('title')
+        title_tag.string = 'دکوراسیون عبدالباسط - بهترین خدمات دکوراسیون در قشم'
+        soup.head.append(title_tag)
+    
     # افزودن متا تگ‌های Open Graph
     og_tags = [
         ('og:title', 'دکوراسیون عبدالباسط - بهترین خدمات دکوراسیون در قشم'),
@@ -66,8 +68,8 @@ def add_meta_tags():
 
     # ذخیره تغییرات در فایل
     index_path.write_text(str(soup), encoding='utf-8')
-
+    
 # فراخوانی تابع
 add_meta_tags()
 
-st.text("ABDOLLAH CHELASI")
+st.text("hi")
